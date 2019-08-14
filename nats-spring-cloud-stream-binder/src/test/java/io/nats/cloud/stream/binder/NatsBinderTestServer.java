a5d3559c840874f6e344a28d1dc1de803f5d4b54
Stephen Asbury <sasbury@synadia.com>
2019-08-14 13:49:57 -0700

    moved gnatsd to nats-server

--- nats-spring-cloud-stream-binder/src/test/java/io/nats/cloud/stream/binder/NatsBinderTestServer.java
+++ nats-spring-cloud-stream-binder/src/test/java/io/nats/cloud/stream/binder/NatsBinderTestServer.java
@@ -31,7 +31,7 @@ import java.util.regex.Pattern;
  */
 public class NatsBinderTestServer implements AutoCloseable {
 
-    private static final String NATS_SERVER = "gnatsd";
+    private static final String NATS_SERVER = "nats-server";
 
     // Use a new port each time, we increment and get so start at the normal port
     private static AtomicInteger portCounter = new AtomicInteger(11223);
