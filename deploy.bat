b29fea0db0fcdf59920e27c6518bfdbf04b42703
scottf <scottfauerbach@gmail.com>
2024-09-13 19:08:04 -0400

    Deployed 0.6.0-3.1

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,10 @@
-@echo Make sure you have the "Build Admin Signing Key Password" from 1password
+@echo --------------------------------------------------------------------------------
+@echo 1. Make sure you have the following from 1password
+@echo "Java GHA SIGNING_PASSWORD Build Admin" - used for gpg signing
+@echo "spring-nats.settings.xml" - used to store sonatype login and gpgp references
+@echo --------------------------------------------------------------------------------
+@echo 2. You must have Java 17 or later
+@echo --------------------------------------------------------------------------------
 pause
 
 call mvn -N clean install
